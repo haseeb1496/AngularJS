05fbbfda694a3c04eeaabbe7025bb8da63a60754
haseeb1496 <usmani_haseeb@yahoo.com>
2018-10-23 19:07:41 +0500

    Angular Tutorials

---
+++ js/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('Tutorial App', ['ngRoute', 'tutorialCtrlModule']);
+
+app.config(function($routeProvider){
+
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'Views/tutorial.html',
+            controller: 'TutorialCtrl'
+        })
+
+        .when('#/tutorialSecond', {
+            templateUrl: 'Views/tutorialSecond.html',
+            controller: 'TutorialCtrl2'
+        })
+
+        .otherwise({
+            redirect:'/'
+        });
+
+});
---
+++ js/controllers/tutorialCtrl.js
@@ -0,0 +1,34 @@
+angular.module("tutorialCtrlModule", [])
+
+.controller('TutorialCtrl', ['$scope', "Calculations", function($scope, Calculations){
+
+        $scope.tutorialObject = {};
+        $scope.tutorialObject.title = 'Main Page';
+        $scope.tutorialObject.subtitle = 'Sub Title';
+        $scope.tutorialObject.bindOutput = 20;
+        $scope.tutorialObject.firstName = "Haseeb Ullah";
+        $scope.tutorialObject.lastName = "Usmani";
+        $scope.timesTwo = function() {
+            $scope.tutorialObject.bindOutput = Calculations.timesTwo($scope.tutorialObject.bindOutput);
+        }
+
+    }])
+
+
+.factory("Calculations", function (){
+
+    var calc = {};
+
+    calc.timesTwo = function(a){
+        return a * 2;
+    };
+
+    return calc;
+
+})
+
+.controller('TutorialCtrl2', [$scope, function($scope){
+
+    $scope.secondTutorial = 'This is the Second Page';
+
+}]);
